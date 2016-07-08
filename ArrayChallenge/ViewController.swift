4e08a2a47e4bd75a7286e878f326ec3160f1ffd2
Uskinn <s.nevzor@yahoo.com>
2016-07-08 14:44:54 -0400

    Done

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,15 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    var listss = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let items = ["Bananas", "Apples", "Eggs", "Rolls"]
+        let quantity = [6, 4, 12, 4]
         
-    
-        
+        self.shoppingList = self.makeShoppingList(items, quantityOfItems: quantity)
+        print(makeShoppingList)
         
     }
     
-    
+    func makeShoppingList(listOfItems: [String], quantityOfItems: [Int]) -> [String]{
+        
+        var resultArray: [String] = []
+        
+        if quantityOfItems.count == listOfItems.count {
+            print(resultArray)
+            for (index, item) in listOfItems.enumerate() {
+                
+                resultArray.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+                print("Results Array - \(resultArray)")
+        }
+        }
+        
+        return resultArray
+    }
     
 }
